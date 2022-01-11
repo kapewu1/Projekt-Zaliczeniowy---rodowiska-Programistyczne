3d8773fc815ac146ae22e09dd0fae92fc2ab1fb2
kamil.jarosz <s5106502@bournemouth.ac.uk>
2022-01-11 21:55:19 +0100

    Implementacja Use Case

---
+++ app/src/main/java/com/example/paweljablonski/cryptoapp/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,31 @@
+package com.example.paweljablonski.cryptoapp.domain.use_case.get_coin
+
+import com.example.paweljablonski.cryptoapp.common.Resource
+import com.example.paweljablonski.cryptoapp.data.remote.dto.toCoinDetail
+import com.example.paweljablonski.cryptoapp.domain.model.Coin
+import com.example.paweljablonski.cryptoapp.domain.model.CoinDetail
+import com.example.paweljablonski.cryptoapp.domain.model.toCoin
+import com.example.paweljablonski.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading<CoinDetail>())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException){
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException){
+            emit(Resource.Error<CoinDetail>("Couldn't reach server. Check your internet connection"))
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/paweljablonski/cryptoapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,29 @@
+package com.example.paweljablonski.cryptoapp.domain.use_case.get_coins
+
+import com.example.paweljablonski.cryptoapp.common.Resource
+import com.example.paweljablonski.cryptoapp.domain.model.Coin
+import com.example.paweljablonski.cryptoapp.domain.model.toCoin
+import com.example.paweljablonski.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading<List<Coin>>())
+            val coins = repository.getCoins().map { it.toCoin()}
+            emit(Resource.Success(coins))
+        } catch (e: HttpException){
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException){
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection"))
+        }
+    }
+
+}
